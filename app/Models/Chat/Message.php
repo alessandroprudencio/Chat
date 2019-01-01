3794ebf04e99a61aca769f5079c29d172ecd9a51
alessandroprudencio <alessandroconectado@gmail.com>
2019-01-01 14:19:11 -0200

    Configurando recuperação de senha

--- app/Models/Chat/Message.php
+++ app/Models/Chat/Message.php
@@ -4,6 +4,8 @@ namespace App\Models\Chat;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Carbon\Carbon;
+
 
 class Message extends Model //retorna msg
 {
@@ -14,8 +16,8 @@ class Message extends Model //retorna msg
         return $this->user_id == auth()->user()->id; // o valor da coluna user_Id na tabela message e iqal ao usuario logado        
     }
 
-    public function  getCreatedAtAttribute($value){
-        return \Carbon\Carbon::parse($value)->format('d/m/Y H:i');
+    public function  getCreateAttribute($value){
+        return Carbon\Carbon::parse($value)->format('d/m/Y H:i');
     }
     
     public function user() //rtorna usuario da mensagem
